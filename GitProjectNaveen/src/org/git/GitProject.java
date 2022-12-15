5b552c9301457d6434de4059a3bafcac203610f0
blacky0303 <kannan.tk1903@gmail.com>
2022-12-15 20:08:15 +0530

    commited to git

---
+++ GitProjectNaveen/src/org/git/GitProject.java
@@ -0,0 +1,10 @@
+package org.git;
+
+public class GitProject {
+	public static void main(String[] args) {
+		System.out.println("this");
+		System.out.println("is");
+		System.out.println("kannan");
+		System.out.println("project");
+	}
+}
